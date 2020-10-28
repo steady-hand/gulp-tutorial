4ab14bb60f5ba8c4eef940c6445612e81a6c15d3
steady-hand <v.gabrusenok10@gmail.com>
2020-10-28 22:01:10 +0300

    добавил модуль для добавления картинок в build и подключил их к карточкам

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 var gulp = require('gulp');
+var changed = require('gulp-changed');
 var nunjucks = require('gulp-nunjucks');
 var sass = require('gulp-sass');
 var browsersync = require('browser-sync');
@@ -9,15 +10,18 @@ var reload = browsersync.reload;
 var path = {
     src: {
         html: 'src/*.html',
-        styles: 'src/styles/*.scss'
+        styles: 'src/styles/*.scss',
+        img: "src/img/**/*.{jpg,jpeg,png,svg}"
     },
     build: {
         html: 'build/',
-        styles: 'build/css/'
+        styles: 'build/css/',
+        img: "build/img/"
     },
     watch: {
         html: 'src/**/*.html',
-        styles: 'src/styles/**/*.scss'
+        styles: 'src/styles/**/*.scss',
+        img: "src/img/**/*.{jpg,jpeg,png,svg}"
     },
     base: './build'
 };
@@ -52,15 +56,25 @@ function styles() {
         .pipe(reload({ stream: true }));
 };
 
+function img() {
+    return gulp
+        .src(path.src.img)
+        .pipe(changed(path.build.img))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({ stream: true }));
+};
+
 function watchFiles() {
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.styles], styles);
+    gulp.watch([path.watch.img], img);
 };
 
 gulp.task('html', html);
 gulp.task('styles', styles);
+gulp.task('img', img);
 
-gulp.task('build', gulp.series(clean, gulp.parallel(html, styles)));
+gulp.task('build', gulp.series(clean, gulp.parallel(html, styles, img)));
 gulp.task('watch', gulp.parallel(watchFiles, browserSync));
 
 
